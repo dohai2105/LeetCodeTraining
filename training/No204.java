42560c76f7c2c4703e758d05a017af1c2c4e75eb
nam.vule <nam.vule>
2021-06-15 16:09:58 +0900

    update

--- training/No204.java
+++ training/No204.java
@@ -1,5 +1,37 @@
+//https://www.geeksforgeeks.org/sieve-of-eratosthenes/
+
 public class No204 {
-    public int countPrimes(int n) {
-        
+    public static int countPrimes(int n) {
+        if(n <= 2) return 0;
+        int count = n/2;
+        boolean []isPrimesMatrix = new boolean[n];
+
+        for (int i = 3; i*i < n; i += 2) {
+            if(isPrimesMatrix[i] == true)
+                continue;
+            for(int j = i*i; j < n; j += 2*i){
+                if(isPrimesMatrix[j] ==  false) {
+                    isPrimesMatrix[j] = true;
+                    count --;
+                }
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        countPrimes(12);
     }
 }
+
+
+
+// Count the number of prime numbers less than a non-negative number, n.
+
+ 
+
+// Example 1:
+
+// Input: n = 10
+// Output: 4
+// Explanation: There are 4 prime numbers less than 10, they are 2, 3, 5, 7.
---
+++ training/No206.java
@@ -0,0 +1,15 @@
+public class No206 {
+
+// Definition for singly-linked list.
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    public ListNode reverseList(ListNode head) {
+        return null;
+    }
+}
