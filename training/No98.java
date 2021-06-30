11e42a7f7d313b505a883250c70dafc751ccbec4
cmchai <cmchai>
2021-06-30 14:03:26 +0900

    update

--- training/No98.java
+++ training/No98.java
@@ -1,30 +1,20 @@
 
 // Definition for a binary tree node.
-class No98 {
 
+public class No98 {
     public static class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-    TreeNode() {}
-    TreeNode(int val) { this.val = val; }
-    TreeNode(int val, TreeNode left, TreeNode right) {
-        this.val = val;
-        this.left = left;
-        this.right = right;
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
     }
-}
-    public boolean isSymmetric(TreeNode root) {
-
-        return compareLeftRight(root, root);
-    }
-
-    public boolean compareLeftRight(TreeNode left, TreeNode right) {
-
-        if(left == null && right == null) return true;
-        if(left == null || right == null) return false;
-        return left.val == right.val && 
-            compareLeftRight(left.left, right.right) &&
-            compareLeftRight(left.right, right.left);
+    public boolean isValidBST(TreeNode root) {
+        return false;
     }
 }
