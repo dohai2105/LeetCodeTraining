84735eb7e1b57f64f7f5bd055c72d3b89a512351
cmchai <cmchai>
2021-06-30 17:18:45 +0900

    udpate

--- training/No98.java
+++ training/No98.java
@@ -15,6 +15,19 @@ public class No98 {
         }
     }
     public boolean isValidBST(TreeNode root) {
-        return false;
+        if(root == null) return true;
+
+        TreeNode leftNode = root.left;
+        TreeNode rightNode = root.right;
+
+        if(leftNode == null && rightNode == null) return true;
+
+        int leftNodeVal = leftNode != null ? leftNode.val : Integer.MIN_VALUE;
+        int rightNodeVal = rightNode != null ? rightNode.val : Integer.MAX_VALUE;
+
+        boolean isValidBST = leftNodeVal < root.val && root.val < rightNodeVal ? true :  false;
+
+        return isValidBST && isValidBST(leftNode) && isValidBST(rightNode);
     }
+    
 }
